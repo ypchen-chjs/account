16f58b012e4eb309dae6851fd4daae0d97be73ab
yiping <jc5230@gmail.com>
2014-02-16 16:48:55 +0800

    2014.02.16

---
+++ account-persist/src/main/java/com/juvenxu/mvnbook/account/persist/AccountPersistException.java
@@ -0,0 +1,11 @@
+package com.juvenxu.mvnbook.account.persist;
+
+import org.dom4j.DocumentException;
+
+public class AccountPersistException extends Exception {
+
+	public AccountPersistException(String string, Exception e) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
